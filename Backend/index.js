ac929718cd96f0201abc2f0111207863f884585b
adab786 <siddiquiadab@eng.rizvi.edu.in>
2024-08-25 02:07:26 +0530

    verceljson

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use("/courses", getCourses);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // mongoose
 //   .connect(process.env.MONGO_URI)
@@ -27,13 +27,13 @@ const PORT = process.env.PORT || 4000;
 //     console.log("Error:", error.message);
 //   });
 
-// app.use((req, res) => {
-//   res.send("Hello World");
-// });
-
-app.get("/", (req, res) => {
+app.use((req, res) => {
   res.send("Hello World");
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
