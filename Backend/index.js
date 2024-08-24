358df001654b1b68ac6b1d7888c1beb78e0ca59f
adab786 <siddiquiadab@eng.rizvi.edu.in>
2024-08-25 02:32:05 +0530

    verceljson4

--- Backend/index.js
+++ Backend/index.js
@@ -27,13 +27,13 @@ const PORT = process.env.PORT || 5000;
 //     console.log("Error:", error.message);
 //   });
 
-app.use((req, res) => {
-  res.send("Hello World");
-});
-
-// app.get("/", (req, res) => {
+// app.use((req, res) => {
 //   res.send("Hello World");
 // });
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
