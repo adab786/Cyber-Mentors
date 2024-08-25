33b71a82dc9fa2caddb3547cc971e38083c4ca53
adab786 <siddiquiadab@eng.rizvi.edu.in>
2024-08-25 20:39:00 +0530

    await

--- Backend/index.js
+++ Backend/index.js
@@ -26,29 +26,28 @@ mongoose
 app.use("/courses", getCourses);
 
 app.get("/course", (req, res) => {
-  const data =
-    await[
-      ({
-        _id: "66cb08f5e4ce4526c5771dbc",
-        title: "Introduction to Graphic Design",
-        Image: "https://example.com/graphic-design.jpg",
-        instructor: "Merlina Port",
-        description:
-          "Learn the basics of graphic design including tools, techniques, and industry best practices. Perfect for beginners looking to get started in design.",
-        schedule: "Weekdays, 10:00 AM - 12:00 PM",
-        price: 104.7,
-      },
-      {
-        _id: "66cb097ee4ce4526c5778239",
-        title: "Advanced Python Programming",
-        Image: "https://example.com/python-programming.jpg",
-        instructor: "Darbie Faltskog",
-        description:
-          "An advanced course on Python programming covering topics such as data analysis, machine learning, and web development. Ideal for those looking to deepen their Python knowledge.",
-        schedule: "Weekends, 2:00 PM - 5:00 PM",
-        price: 64.27,
-      })
-    ];
+  const data = [
+    ({
+      _id: "66cb08f5e4ce4526c5771dbc",
+      title: "Introduction to Graphic Design",
+      Image: "https://example.com/graphic-design.jpg",
+      instructor: "Merlina Port",
+      description:
+        "Learn the basics of graphic design including tools, techniques, and industry best practices. Perfect for beginners looking to get started in design.",
+      schedule: "Weekdays, 10:00 AM - 12:00 PM",
+      price: 104.7,
+    },
+    {
+      _id: "66cb097ee4ce4526c5778239",
+      title: "Advanced Python Programming",
+      Image: "https://example.com/python-programming.jpg",
+      instructor: "Darbie Faltskog",
+      description:
+        "An advanced course on Python programming covering topics such as data analysis, machine learning, and web development. Ideal for those looking to deepen their Python knowledge.",
+      schedule: "Weekends, 2:00 PM - 5:00 PM",
+      price: 64.27,
+    }),
+  ];
 
   res.json(data);
 });
