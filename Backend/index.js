07af75af0af156d93ca39e4fc37f0175d3f8dedc
adab786 <siddiquiadab@eng.rizvi.edu.in>
2024-08-25 20:35:13 +0530

    routing

--- Backend/index.js
+++ Backend/index.js
@@ -24,27 +24,55 @@ mongoose
     console.log("Error:", error.message);
   });
 app.use("/courses", getCourses);
-app.get("/users", (req, res) => {
-  // console.log("Received request at /users");
-  try {
-    const data = [
-      {
-        name: "John Doe",
-        age: 25,
-        email: "zdc@gmail.com",
+
+app.get("/course", (req, res) => {
+  const data =
+    await[
+      ({
+        _id: "66cb08f5e4ce4526c5771dbc",
+        title: "Introduction to Graphic Design",
+        Image: "https://example.com/graphic-design.jpg",
+        instructor: "Merlina Port",
+        description:
+          "Learn the basics of graphic design including tools, techniques, and industry best practices. Perfect for beginners looking to get started in design.",
+        schedule: "Weekdays, 10:00 AM - 12:00 PM",
+        price: 104.7,
       },
       {
-        name: "Jane Doe",
-        age: 24,
-        email: "adx@gmail.com",
-      },
+        _id: "66cb097ee4ce4526c5778239",
+        title: "Advanced Python Programming",
+        Image: "https://example.com/python-programming.jpg",
+        instructor: "Darbie Faltskog",
+        description:
+          "An advanced course on Python programming covering topics such as data analysis, machine learning, and web development. Ideal for those looking to deepen their Python knowledge.",
+        schedule: "Weekends, 2:00 PM - 5:00 PM",
+        price: 64.27,
+      })
     ];
-    res.json(data);
-    // res.send("data");
-  } catch (error) {
-    console.log("Error:", error.message);
-  }
+
+  res.json(data);
 });
+// app.get("/users", (req, res) => {
+//   // console.log("Received request at /users");
+//   try {
+//     const data = [
+//       {
+//         name: "John Doe",
+//         age: 25,
+//         email: "zdc@gmail.com",
+//       },
+//       {
+//         name: "Jane Doe",
+//         age: 24,
+//         email: "adx@gmail.com",
+//       },
+//     ];
+//     res.json(data);
+//     // res.send("data");
+//   } catch (error) {
+//     console.log("Error:", error.message);
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
